d2f7a3fd6565925727333261cb47bc3003e54f4a
Spyridon Passas <spyridonpassas0325@gmail.com>
2026-09-15 11:00:07 +0200

    feat: update navbar to match Nordic Nocturne design and respect browser theme

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -3,50 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'app-navbar',
-  styles: `
-    .navbar {
-      justify-content: space-between;
-      padding-left: 1rem;
-      padding-right: 1rem;
-    }
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
 
-    .navbar-body {
-      gap: 1rem;
-    }
-
-    .navbar-context-body {
-      gap: 1rem;
-    }
-
-    .u-align-h {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
-
-    .u-clear-list {
-      list-style: none;
-    }
-  `,
-  template: `
-    <div class="navbar u-align-h u-clear-list">
-      <div class="u-align-h">
-        <h1>Book Site</h1>
-        <nav>
-          <ul class="navbar-body u-align-h u-clear-list">
-            <li><button routerLink="/home">Books</button></li>
-            <li><button routerLink="/about">My Quotes</button></li>
-          </ul>
-        </nav>
-      </div>
-
-      <ul class="navbar-context-body u-align-h u-clear-list">
-        <li><button routerLink="/register">EN/SV</button></li>
-        <li><button routerLink="/register">Theme</button></li>
-        <li><button routerLink="/login">Login</button></li>
-      </ul>
-    </div>
-  `,
 })
 export class Navbar {}
 
